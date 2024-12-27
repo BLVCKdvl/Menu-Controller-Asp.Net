78d1ff184945be245c0c5b846707d58f0c86edad
Dmitry <s20.rex9000@gmail.com>
2024-12-27 06:47:27 +0300

    Add project files.

---
+++ Reustarant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reustarant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
